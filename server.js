55a6c6319d557ec151f5d604d7b52a9e4c9613e5
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-17 14:05:16 -0700

    add connection to product routes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('./server/config/mongoose.config'); //
 app.use(cors());
 app.use(express.json()); // This is new
 app.use(express.urlencoded({ extended: true })); // This is new
-require('./server/routes/person.routes')(app); // Loads available routes
+require('./server/routes/product.routes')(app); // Loads available routes
 app.listen(8080, () => {
 	console.log('Listening at Port 8080');
 });
