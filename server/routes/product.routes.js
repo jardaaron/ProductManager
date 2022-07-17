95df836b63cc04191064cf2f67397b6573cdeeb3
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-17 15:33:36 -0700

    add findallproduct route

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,5 +1,6 @@
 const ProductController = require('../controllers/product.controller');
 module.exports = function (app) {
 	app.get('/api', ProductController.index);
+	app.get('/api/product', ProductController.findAllProducts);
 	app.post('/api/product', ProductController.createProduct);
 };
